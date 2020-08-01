53311b10a6eb689b22784862af6087f50391fa99
Jose Luis Rosas Peimbert <jlrosasp@ipera4.com>
2020-08-01 13:24:33 -0500

    Se agrega disenio inicial de la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navigation/navbar/navbar.component';
+import { FooterComponent } from './components/navigation/footer/footer.component';
+import { SidebarComponent } from './components/navigation/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
